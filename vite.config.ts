37598911c247f6b47567d77c046ff84c38d8c350
Matheus Neves <matneves@SAO-C02G620KML7H.local>
2022-08-10 18:50:52 -0300

    chore: add path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: resolve(__dirname, 'src') },
+      { find: '@components', replacement: resolve(__dirname, 'src/components') },
+      { find: '@styles', replacement: resolve(__dirname, 'src/styles') },
+      { find: '@pages', replacement: resolve(__dirname, 'src/pages') }
+    ],
+  },
 })
