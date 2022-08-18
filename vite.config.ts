dba360c282868cc118e578bcdbe87efb0632c0b0
Matheus Neves <matheusneves1989@gmail.com>
2022-08-17 22:39:46 -0300

    chore: add styles alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [
-      { find: '@', replacement: resolve(__dirname, 'src') },
-      { find: '@components', replacement: resolve(__dirname, 'src/components') },
-      { find: '@styles', replacement: resolve(__dirname, 'src/styles') },
-      { find: '@pages', replacement: resolve(__dirname, 'src/pages') }
+      { find: '@src', replacement: resolve(__dirname, 'src') },
+      {
+        find: '@components',
+        replacement: resolve(__dirname, 'src', 'components'),
+      },
+      { find: '@styles', replacement: resolve(__dirname, 'src', 'styles') },
+      { find: '@pages', replacement: resolve(__dirname, 'src', 'pages') },
+      { find: '@styles', replacement: resolve(__dirname, 'src', 'styles') },
     ],
   },
-})
+});
