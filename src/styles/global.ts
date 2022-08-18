051be9e8f557921177bc02f1a2e772d1574b3271
Matheus Neves <matheusneves1989@gmail.com>
2022-08-17 22:43:09 -0300

    feat(Styles): create global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 4px 2px ${({ theme }) => theme['gray-500']};
+  }
+
+  body {
+    background-color: ${({ theme }) => theme['gray-100']};
+    color: ${({ theme }) => theme['black-400']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+
+`;
