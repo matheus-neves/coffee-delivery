8435a7b97d373b16a1a85a1bae92722985130c1c
Matheus Neves <matheusneves1989@gmail.com>
2022-09-13 22:32:17 -0300

    feat(Theme): add breakpoints

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,21 +1,28 @@
 export const defaultTheme = {
-  'yellow-100': '#F1E9C9',
-  'yellow-500': '#DBAC2C',
-  'yellow-700': '#C47F17',
+  pallete: {
+    'yellow-100': '#F1E9C9',
+    'yellow-500': '#DBAC2C',
+    'yellow-700': '#C47F17',
 
-  'purple-100': '#EBE5F9',
-  'purple-500': '#8047F8',
-  'purple-700': '#4B2995',
+    'purple-100': '#EBE5F9',
+    'purple-500': '#8047F8',
+    'purple-700': '#4B2995',
 
-  'black-400': '#574F4D',
-  'black-500': '#403937',
-  'black-800': '#272221',
+    'black-400': '#574F4D',
+    'black-500': '#403937',
+    'black-800': '#272221',
 
-  'gray-100': '#FAFAFA',
-  'gray-200': '#F3F2F2',
-  'gray-300': '#EDEDED',
-  'gray-400': '#E6E5E5',
-  'gray-500': '#D7D5D5',
+    'gray-100': '#FAFAFA',
+    'gray-200': '#F3F2F2',
+    'gray-300': '#EDEDED',
+    'gray-400': '#E6E5E5',
+    'gray-500': '#D7D5D5',
 
-  white: '#FFFFFF',
+    white: '#FFFFFF',
+  },
+  breakpoints: {
+    sm: 600,
+    md: 900,
+    lg: 1120
+  }
 };
