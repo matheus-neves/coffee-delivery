91332239ae9df61559e9fc3fec46bf289664ab85
Matheus Neves <matheusneves1989@gmail.com>
2022-09-27 17:33:45 -0300

    refactor(Header): use CartLink component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,13 @@
 import Logo from '@assets/logo.svg';
+import {
+  ActionsContainer,
+  HeaderContainer,
+  LocationIndicator
+} from '@components/Header/styles';
+import { CartLink } from '@src/components/CartLink';
 import { MapPin, ShoppingCart } from 'phosphor-react';
 import { Link } from 'react-router-dom';
 import { useTheme } from 'styled-components';
-import {
-  ActionsContainer,
-  CardButton,
-  HeaderContainer,
-  LocationIndicator
-} from './styles';
 
 export function Header() {
   const { pallete } = useTheme();
@@ -22,10 +22,10 @@ export function Header() {
           <MapPin weight="fill" color={pallete['purple-500']} />
           <span>Porto Alegre, RS</span>
         </LocationIndicator>
-        <CardButton>
+        <CartLink to={'/'} bgColor={pallete['yellow-100']}>
           <ShoppingCart weight="fill" color={pallete['yellow-700']} size={22} />
           <span>3</span>
-        </CardButton>
+        </CartLink>
       </ActionsContainer>
     </HeaderContainer>
   );
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -26,35 +26,3 @@ export const LocationIndicator = styled.div`
 
   font-size: ${pxToRem(14)};
 `;
-
-export const CardButton = styled.button`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-  width: ${pxToRem(38)};
-  height: ${pxToRem(38)};
-  padding: ${pxToRem(8)};
-  background-color: ${({ theme }) => theme.pallete['yellow-100']};
-  border-radius: 4px;
-  border-width: 0;
-
-  span {
-    position: absolute;
-    top: -8px;
-    right: -8px;
-    width: ${pxToRem(20)};
-    height: ${pxToRem(20)};
-    border-radius: 50%;
-
-    background-color: ${({ theme }) => theme.pallete['yellow-700']};
-    color: ${({ theme }) => theme.pallete['white']};
-
-    font-weight: 700;
-    font-size: ${pxToRem(12)};
-
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-`;
