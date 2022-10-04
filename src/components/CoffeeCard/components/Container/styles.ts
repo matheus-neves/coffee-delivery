f478b5c8af4e5165492c4c3a2bd9037cb4bcba4e
Matheus Neves <matheusneves1989@gmail.com>
2022-10-04 20:49:48 -0300

    feat(CoffeeCard): style changes

--- src/components/CoffeeCard/components/Container/styles.ts
+++ src/components/CoffeeCard/components/Container/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const CoffeeCardContainer = styled.li`
   background-color: ${({ theme }) => theme.pallete['gray-200']};
   border-radius: 6px 36px;
-  padding: 0 2rem 2rem;
+  padding: 0 2.4rem 2.4rem;
   max-width: 25.6rem;
   width: 100%;
 
