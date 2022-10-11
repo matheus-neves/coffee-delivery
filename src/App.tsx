89fb15f676ec582cb29ebb3ad9bd8114775248dd
Matheus Neves <matheusneves1989@gmail.com>
2022-10-11 19:53:20 -0300

    refactor(Router): update react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
+import { router } from '@src/routes/Router';
 import { GlobalStyles } from '@styles/global';
 import { defaultTheme } from '@styles/themes/default';
+import { RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import { Router } from './routes/Router';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
-      <Router />
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 }
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,15 +1,26 @@
+import { Checkout } from '@pages/Checkout';
 import { Home } from '@pages/Home';
 import { DefaultLayout } from '@src/layouts/DefaultLayout';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Success } from '@src/pages/Success';
+import { createBrowserRouter } from 'react-router-dom';
 
-export function Router() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<DefaultLayout />}>
-          <Route path="/" element={<Home />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <DefaultLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: 'checkout',
+        element: <Checkout />
+      },
+      {
+        path: 'checkout/success',
+        element: <Success />
+      }
+    ]
+  }
+]);
