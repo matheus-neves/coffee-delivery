9a25060fced29066ebd36e87b8809f5a9b308ece
Matheus Neves <matheusneves1989@gmail.com>
2022-10-23 14:19:32 -0300

    feat(Home): fetch coffee data

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -18,10 +18,27 @@ import {
 } from 'phosphor-react';
 import { useTheme } from 'styled-components';
 
-import traditionalCoffee from '@assets/coffees/traditional.png';
+import { CoffeeState } from '@pages/Home/types';
+import { api } from '@src/services/api';
+import { formatNumber } from '@src/utils/formatNumber';
+import { useEffect, useState } from 'react';
 
 export function Home() {
   const { pallete } = useTheme();
+  const [coffees, setCoffees] = useState<CoffeeState[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await api.get<CoffeeState[]>('coffees');
+      const formattedData = data.map(coffee => ({
+        ...coffee,
+        formattedPrice: formatNumber({
+          value: coffee.price
+        })
+      }));
+      setCoffees(formattedData);
+    })();
+  }, []);
 
   return (
     <main>
@@ -75,41 +92,18 @@ export function Home() {
       <CoffeeSection>
         <h2>Our coffees</h2>
         <CoffeeList>
-          <CoffeeCard
-            description="O tradicional café feito com água quente e grãos moídos"
-            image={traditionalCoffee}
-            price={9.9}
-            tags={['Traditional', 'teste']}
-            title="Expresso Tradicional"
-          />
-          <CoffeeCard
-            description="O tradicional café feito com água quente e grãos moídos"
-            image={traditionalCoffee}
-            price={9.9}
-            tags={['Traditional', 'teste']}
-            title="Expresso Tradicional"
-          />
-          <CoffeeCard
-            description="O tradicional café feito com água quente e grãos moídos"
-            image={traditionalCoffee}
-            price={9.9}
-            tags={['Traditional', 'teste']}
-            title="Expresso Tradicional"
-          />
-          <CoffeeCard
-            description="O tradicional café feito com água quente e grãos moídos"
-            image={traditionalCoffee}
-            price={9.9}
-            tags={['Traditional', 'teste']}
-            title="Expresso Tradicional"
-          />
-          <CoffeeCard
-            description="O tradicional café feito com água quente e grãos moídos"
-            image={traditionalCoffee}
-            price={9.9}
-            tags={['Traditional', 'teste']}
-            title="Expresso Tradicional"
-          />
+          {coffees.map(coffee => (
+            <CoffeeCard
+              id={coffee.id}
+              key={coffee.id}
+              description={coffee.description}
+              src={coffee.src}
+              price={coffee.price}
+              formattedPrice={coffee.formattedPrice}
+              tags={coffee.tags}
+              title={coffee.title}
+            />
+          ))}
         </CoffeeList>
       </CoffeeSection>
     </main>
--- src/pages/Home/types.ts
+++ src/pages/Home/types.ts
@@ -1,3 +1,13 @@
 export interface IconContainerProps {
   bgcolor: string;
 }
+
+export interface CoffeeState {
+  id: number;
+  src: string;
+  tags: string[];
+  title: string;
+  description: string;
+  price: number;
+  formattedPrice: string;
+}
