574b3615f195921050eda573d71ad95041c9219e
Matheus Neves <matheusneves1989@gmail.com>
2022-10-23 13:31:33 -0300

    build: add services alias

--- vite.config.ts
+++ vite.config.ts
@@ -10,12 +10,17 @@ export default defineConfig({
       { find: '@src', replacement: resolve(__dirname, 'src') },
       {
         find: '@components',
-        replacement: resolve(__dirname, 'src', 'components'),
+        replacement: resolve(__dirname, 'src', 'components')
       },
       { find: '@styles', replacement: resolve(__dirname, 'src', 'styles') },
       { find: '@pages', replacement: resolve(__dirname, 'src', 'pages') },
       { find: '@styles', replacement: resolve(__dirname, 'src', 'styles') },
       { find: '@assets', replacement: resolve(__dirname, 'src', 'assets') },
-    ],
+      { find: '@services', replacement: resolve(__dirname, 'src', 'services') }
+    ]
   },
+  server: {
+    host: true,
+    port: 3000
+  }
 });
