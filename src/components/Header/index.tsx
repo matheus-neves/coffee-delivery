a347945bdac9847af2bfc0f4b1e7958a0d9fbe8c
Matheus Neves <matheusneves1989@gmail.com>
2022-11-07 16:39:19 -0300

    feat(Header): change class at header using intersection observer

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,15 @@
 import Logo from '@assets/logo.svg';
-import { ActionsContainer, HeaderContainer } from '@components/Header/styles';
+import {
+  ActionsContainer,
+  HeaderContainer,
+  HeaderContent,
+  SentinelBox
+} from '@components/Header/styles';
 import { LocationIndicator } from '@components/LocationIndicator';
 import { CartLink } from '@src/components/CartLink';
 import { useCartContext } from '@src/contexts/CartContext';
 import { ShoppingCart } from 'phosphor-react';
+import { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { useTheme } from 'styled-components';
 
@@ -11,18 +17,56 @@ export function Header() {
   const { pallete } = useTheme();
   const { cartItems } = useCartContext();
 
+  const headerRef = useRef<HTMLHeadingElement>(null);
+  const sentinelEl = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      entries => {
+        const entry = entries[0];
+        const targetInfo = entry.boundingClientRect;
+        if (targetInfo.top < 0) {
+          return headerRef?.current?.classList.add('shrink-shadow');
+        }
+        headerRef?.current?.classList.remove('shrink-shadow');
+      },
+      {
+        threshold: 1
+      }
+    );
+
+    if (sentinelEl.current) {
+      observer.observe(sentinelEl.current);
+    }
+
+    return () => {
+      if (sentinelEl.current) {
+        observer.unobserve(sentinelEl.current);
+      }
+    };
+  }, []);
+
   return (
-    <HeaderContainer>
-      <Link to="/">
-        <img src={Logo} alt="Coffee Delivery" />
-      </Link>
-      <ActionsContainer>
-        <LocationIndicator />
-        <CartLink to={'/checkout'} bgcolor={pallete['yellow-100']}>
-          <ShoppingCart weight="fill" color={pallete['yellow-700']} size={22} />
-          <span>{cartItems.length}</span>
-        </CartLink>
-      </ActionsContainer>
-    </HeaderContainer>
+    <>
+      <SentinelBox ref={sentinelEl} />
+      <HeaderContainer ref={headerRef}>
+        <HeaderContent>
+          <Link to="/">
+            <img src={Logo} alt="Coffee Delivery" />
+          </Link>
+          <ActionsContainer>
+            <LocationIndicator />
+            <CartLink to={'/checkout'} bgcolor={pallete['yellow-100']}>
+              <ShoppingCart
+                weight="fill"
+                color={pallete['yellow-700']}
+                size={22}
+              />
+              <span>{cartItems.length}</span>
+            </CartLink>
+          </ActionsContainer>
+        </HeaderContent>
+      </HeaderContainer>
+    </>
   );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,10 +1,43 @@
 import styled from 'styled-components';
 
 export const HeaderContainer = styled.header`
+  position: sticky;
+  top: 0;
+  z-index: 50;
+  background-color: ${({ theme }) => theme.pallete['gray-100']};
+  padding: 2rem 1rem;
+
+  transition: padding 0.3s ease-out;
+
+  &.shrink-shadow {
+    box-shadow: 0 2px 4px 2px rgba(0, 0, 0, 0.25);
+    padding: 1rem 1rem;
+  }
+
+  @media (min-width: ${({ theme }) => `${theme.breakpoints.sm}px`}) {
+    padding: 3.2rem 2rem;
+
+    &.shrink-shadow {
+      padding: 1.5rem 2rem;
+    }
+  }
+`;
+
+export const SentinelBox = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 20px;
+`;
+
+export const HeaderContent = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 3.2rem 0;
+  max-width: calc(1120px);
+  margin: 0 auto;
+  @media (min-width: ${({ theme }) => `${theme.breakpoints.sm}px`}) {
+    max-width: calc(1120px);
+  }
 `;
 
 export const ActionsContainer = styled.div`
