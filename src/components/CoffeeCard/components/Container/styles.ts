239539d6412b7c7d0df1ffd714b709f54476ee4b
Matheus Neves <matheusneves1989@gmail.com>
2022-11-08 13:58:12 -0300

    fix(CoffeeCard): align cart items

--- src/components/CoffeeCard/components/Container/styles.ts
+++ src/components/CoffeeCard/components/Container/styles.ts
@@ -1,11 +1,15 @@
 import styled from 'styled-components';
 
 export const CoffeeCardContainer = styled.li`
-  background-color: ${({ theme }) => theme.pallete['gray-200']};
-  border-radius: 6px 36px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   padding: 0 2.4rem 2.4rem;
   max-width: 25.6rem;
   width: 100%;
+
+  background-color: ${({ theme }) => theme.pallete['gray-200']};
+  border-radius: 6px 36px;
 
   h3 {
     font-size: 2rem;
