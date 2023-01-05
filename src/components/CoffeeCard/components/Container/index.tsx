137ad0690c920601694151b6e5b83c4d724ff362
Matheus Neves <matheusneves1989@gmail.com>
2023-01-05 15:08:51 -0300

    feat(CoffeeCard): add animations

--- src/components/CoffeeCard/components/Container/index.tsx
+++ src/components/CoffeeCard/components/Container/index.tsx
@@ -1,7 +1,18 @@
+import { Variants } from 'framer-motion';
 import { CoffeeCardContainer } from './styles';
 
 type ContainerProps = { children: React.ReactNode };
 
+const coffeeItem: Variants = {
+  hidden: { translateX: -40, opacity: 0 },
+  visible: {
+    translateX: 0,
+    opacity: 1
+  }
+};
+
 export function Container({ children }: ContainerProps) {
-  return <CoffeeCardContainer>{children}</CoffeeCardContainer>;
+  return (
+    <CoffeeCardContainer variants={coffeeItem}>{children}</CoffeeCardContainer>
+  );
 }
--- src/components/CoffeeCard/components/Container/styles.ts
+++ src/components/CoffeeCard/components/Container/styles.ts
@@ -1,6 +1,7 @@
+import { motion } from 'framer-motion';
 import styled from 'styled-components';
 
-export const CoffeeCardContainer = styled.li`
+export const CoffeeCardContainer = styled(motion.li)`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
